0bd1872d051490720869c2285afdbc4755b43cde
Paulo Freitas <pfreitas1@gmail.com>
2024-01-12 11:39:51 -0500

    wip

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import { Outlet } from "react-router-dom";
-import { GlobalStyles } from '@mui/material';
 import { GlobalStyle } from '../styles/GlobalStyle';
 
 export const Layout: React.FC = () => {
   return (
     <Box sx={{ display: 'flex' }}>
-        <GlobalStyle />
+      <GlobalStyle />
       <Box component="main" sx={{ width: 'calc(100vw - 64px)', p: 3, position: 'relative', left: '64px' }}>
         <Outlet />
       </Box>
